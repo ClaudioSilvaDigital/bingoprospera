769ed0226486ceafdaa0b069b9ec105821c00e3b
ClaudioSilvaDigital <98329076+ClaudioSilvaDigital@users.noreply.github.com>
2025-10-20 22:23:31 -0300

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',     // substitui o "next export"
-  // opcional: se usar imagens do Next, desabilite otimização:
-  // images: { unoptimized: true },
-};
+const nextConfig = { output: 'export' };
 module.exports = nextConfig;
