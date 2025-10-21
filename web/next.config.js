c041c347ff8e729f2e071e552ecfa5c8284ac83f
ClaudioSilvaDigital <98329076+ClaudioSilvaDigital@users.noreply.github.com>
2025-10-20 21:50:49 -0300

    Create next.config.js

---
+++ web/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',     // substitui o "next export"
+  // opcional: se usar imagens do Next, desabilite otimização:
+  // images: { unoptimized: true },
+};
+module.exports = nextConfig;
